d042b2125ffdc108e8c50463ee3813f229439506
Alvaro Muhlethaler <amuhlethaler@vairix.com>
2014-05-15 17:40:26 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleStripeProject::Application.config.secret_key_base = 'c4e77cfd5f3e75f34b2076d7e71f22b0c19910f154b47a0512c2a2f93cf78694a64075fecf3face96bd0dac3274979fcc88c6b05498a42184a90437dc2d25f69'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SampleStripeProject::Application.config.session_store :cookie_store, key: '_sample_stripe_project_session'
